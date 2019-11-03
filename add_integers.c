b2d00b1a5b8de6294b443709186540e67be68c0b
Dibyadarshan Rath <42318933+dibyadarshan8@users.noreply.github.com>
2019-11-03 22:37:03 +0530

    Add files via upload

---
+++ add_integers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	//add two integers.
+	int a,b;
+	printf("Enter two integers a and b respectively:");
+	scanf("%d %d",&a,&b);
+	printf("Sum=%d\n",a+b);
+	return 0;
+}
