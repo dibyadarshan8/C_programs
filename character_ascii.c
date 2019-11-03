9b9499550045e414e3ebf3be6e2fa7b3cc496054
Dibyadarshan Rath <42318933+dibyadarshan8@users.noreply.github.com>
2019-11-03 22:40:27 +0530

    Add files via upload

---
+++ character_ascii.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	//find ASCII value of a character.
+	char ch;
+	printf("Enter a character:");
+	scanf("%c",&ch);
+	printf("The ASCII value of the entered character=%d\n",ch);
+	return 0;
+}
